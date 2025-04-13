1a31a8a88545e845af5b002c9f45daa3135a0b4e
ahmadh9 <ahmad.vava@gmail.com>
2025-04-13 16:13:11 +0300

    Assignment1--backend

--- backend/lecture1/index.js
+++ backend/lecture1/index.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 
 
 
-// fs.writeFile("message.txt", "Hello from node",(err) => {
-//     if(err) throw err;
-//     console.log("the file has been saved");
-// });
+fs.writeFile("message.txt", "Hello from node",(err) => {
+    if(err) throw err;
+    console.log("the file has been saved");
+});
 
 
 
